06c03aaff7a7cf736fe3c0739134ce1caca0d4da
gino097 <gino.gongora097@gmail.com>
2018-10-29 15:41:30 -0500

    info php

--- info.php
+++ info.php
@@ -1,10 +1,7 @@
 <?php
 
-// Muestra toda la informacion, por omision INFO_ALL
 phpinfo();
 
-// Mostrar solo la informacion de modulos.
-// phpinfo(8) produce el mismo resultado.
 phpinfo(INFO_MODULES);
 
 ?>
